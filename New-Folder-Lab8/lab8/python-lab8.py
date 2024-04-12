544e26ee7abbd54d804141ec17efea8718c3213f
Olivia Byram <ocb3wv@virginia.edu>
2024-04-12 18:35:27 +0000

    Final Python COde

--- New-Folder-Lab8/lab8/python-lab8.py
+++ New-Folder-Lab8/lab8/python-lab8.py
@@ -9,8 +9,9 @@ with open('data/schacon.repos.json', 'r') as file:
 for r in data[0:5]:
     data2 = (r['name'], r['html_url'], r['updated_at'], r['visibility'])
     data4 = ','.join(data2)
+    print(data4)
     file1 = open("chacon.csv", "a")
-    file1.write(data4)
+    file1.write(data4 + '\n')
     file1.close()
 
 f = open("chacon.csv", "r")
